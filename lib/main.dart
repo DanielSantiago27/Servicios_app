1ecca1ab8ad355a308e2ba5cf5a6c3b6f3f147f9
DanielSantiago27 <daniel_1322134141@uptecamac.edu.mx>
2025-07-01 13:02:43 -0600

    cambio 5

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 
 
 Future<void> fetchData() async {
-  final response = await http.get(Uri.parse('http://localhost/api.php'));
+  final response = await http.get(Uri.parse('https://servicios-app-1.onrender.com/api.php'));
 
   if (response.statusCode == 200) {
     List data = json.decode(response.body);
@@ -20,7 +20,8 @@ void main() {
   runApp(PagosApp());
 }
 
-const String apiBaseUrl = 'http://localhost/pagosapp/api/';
+const String apiBaseUrl = 'https://servicios-app-1.onrender.com/pagosapp/api/';
+
 
 class PagosApp extends StatelessWidget {
   final Color primaryColor = Color(0xFF1976D2); 
